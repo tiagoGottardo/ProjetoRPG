5b049453a9f97579b31cde36049d8e74246ac502
TiagoGottardo <Tiago.panizio@hotmail.com>
2021-10-16 13:57:25 -0400

    criado o resgate da Url da imagem.

--- routes/maps.js
+++ routes/maps.js
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { View, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native';
-//import { createDrawerNavigator } from '@react-navigation/drawer';
-//import { NavigationContainer } from '@react-navigation/native';
+import { View, StyleSheet, TouchableOpacity, Text, Alert, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as firebase from 'firebase';
 
 function MapsScreen({ navigation }) {
+  const database = firebase.firestore();
+  const [map, setMap] = useState([]);
+
+  useEffect(() => {
+    database.collection("maps").onSnapshot((query)=> {
+      const list = []
+      query.forEach((doc) => {
+        list.push({ ...doc.data(), id: doc.id })
+      })
+      setMap(list)
+    })
+  }, [])
+
+
 
   useEffect(() => {
     (async () => {
@@ -19,21 +31,22 @@ function MapsScreen({ navigation }) {
     })();
   }, []);
 
-  let imageID = 0;
+  let imageID = Date.now();
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
-      quality: 1,
+      quality: 1
     });
 
     console.log(result);
 
+    var imageEndName = Date.now();
+
     if (!result.cancelled) {
-      imageID++;
-        uploadImage(result.uri, ("map"+ imageID))
+        uploadImage(result.uri, ("map"+ imageEndName))
         .then(() => {
           Alert.alert('Success');
         })
@@ -43,41 +56,55 @@ function MapsScreen({ navigation }) {
       }
     }
 
-    const uploadImage = async (uri, imageName) => {
+    const uploadImage = async (uri, mapName) => {
       const response = await fetch(uri);
       const blob = await response.blob();
 
-      var ref = firebase.storage().ref().child("images/"+ imageName);
-      return ref.put(blob);
+      var refMap = firebase.storage().ref().child("images").child(mapName).put(blob).then(() => {
+        firebase.storage().ref().child("images").child(mapName).getDownloadURL().then((url_image) => {
+          console.log("Uri: "+ url_image);
+        });
+      });
+
+      return refMap
     }
+
   
     return (
 
 
 
+    
     <View style={styles.container}>
-    <TouchableOpacity style={styles.addImageButton} onPress={pickImage} >
-      <Text style={{ color: 'white', fontSize: 20, fontWeight:"bold", marginTop: -2}}>+</Text>
-    </TouchableOpacity>
-</View> 
+      <View>
+        <Image
+            source={{ uri: "#" }}
+            style={{ width: 300, height: 300 }}
+          />
+      </View>
+      <View style={ { flex: 1, flexDirection: 'column-reverse', alignItems: 'flex-end', marginRight: 25, } } >
+        <TouchableOpacity style={styles.addImageButton} onPress={pickImage} >
+          <Text style={{ color: 'white', fontSize: 20, fontWeight:"bold", marginTop: -2}}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column-reverse',
-    alignSelf: 'flex-end',
-    marginBottom: 25,
-    marginRight: 25,
   },
   addImageButton: {
+
     width:60,
     height:60,
     backgroundColor:"#1E90FF",
     borderRadius:30,
     justifyContent: 'center',
     alignItems: 'center',
+    position: 'absolute',
+    marginBottom: 25
   }
 });
 
